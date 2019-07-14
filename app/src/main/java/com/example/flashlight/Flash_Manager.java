3f5997df5a8415aa0f8d7089d4656a64d052a517
unknown <sinasoheili79@gmail.com>
2019-07-14 23:50:14 +0430

    added sos button to layout and sos function in flash manager class

--- app/src/main/java/com/example/flashlight/Flash_Manager.java
+++ app/src/main/java/com/example/flashlight/Flash_Manager.java
@@ -5,12 +5,16 @@ import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Flash_Manager
 {
     private CameraManager cm;
     private Context context;
     private String camera_id;
     private boolean is_flash_on;
+    private Timer t;
 
     public Flash_Manager(Context context)
     {
@@ -68,6 +72,39 @@ public class Flash_Manager
     public boolean is_flash_on()
     {
         return this.is_flash_on;
+    }
+
+    public void sos(boolean enable , final Light_Button_View_Background lbvb)
+    {
+        if(enable == true)
+        {
+            t = new Timer();
+            t.scheduleAtFixedRate(new TimerTask()
+            {
+                @Override
+                public void run()
+                {
+                    turn_on_flash();
+                    lbvb.set_light_on();
+                    try
+                    {
+                        Thread.sleep(100);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    turn_off_flash();
+                    lbvb.set_light_off();
+                }
+            } , 0 , 300);
+        }
+        else
+        {
+            t.cancel();
+            turn_off_flash();
+            lbvb.set_light_off();
+        }
     }
 }
 
--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -9,16 +9,23 @@ import android.content.DialogInterface;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     Light_Button_View_Background lbvb ;
     ToggleButton tglbtn;
+    TextView tv_sos;
     Flash_Manager fm;
+    boolean sos_enable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lbvb = findViewById(R.id.background);
         tglbtn = findViewById(R.id.btn_light);
         tglbtn.setOnClickListener(this);
+        tv_sos = findViewById(R.id.tv_sos);
+        tv_sos.setOnClickListener(this);
         fm = new Flash_Manager(this);
 
         if(fm.has_system_flash() == false)
@@ -62,6 +71,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             {
                 lbvb.set_light_off();
                 fm.turn_off_flash();
+            }
+        }
+        else if(view.getId() == tv_sos.getId())
+        {
+            if(sos_enable == false)
+            {
+                fm.sos(true , lbvb);
+                sos_enable = true;
+            }
+            else
+            {
+                fm.sos(false , lbvb);
+                sos_enable = false;
             }
         }
     }
